2f13195f8511af37b60b4a2b0b84068a913c8c02
Sabotage84 <sabotage84@mail.ru>
2022-03-24 23:40:13 +0300

    ADD ch2

---
+++ Ch2/IlDasm/IlDasm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IlDasm
+{
+    
+    class Program
+    {
+        static int a = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello"+a.ToString());
+            Console.ReadLine();
+        }
+
+        static void MM()
+        {
+
+        }
+    }
+}
