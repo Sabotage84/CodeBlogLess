78b32c2cac54a7c8d20bd09ecaba4e05f7e4fa8e
Sabotage84 <sabotage84@mail.ru>
2022-03-30 00:07:02 +0300

    ADD ch4 employee

---
+++ Ch4/Chap4/Chap4/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap4
+{
+    class Employee
+    {
+        int a;
+        public Employee()
+        {
+            a = 5;
+        }
+        public Employee(int _a)
+        {
+            a = _a;
+        }
+
+        public void CallEpmloyee()
+        {
+            Console.WriteLine("Emploee class!");
+        }
+    }
+}
