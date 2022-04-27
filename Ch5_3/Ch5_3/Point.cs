d5569a898a88f841095bde681ad8e527986d0812
Sabotage84 <sabotage84@mail.ru>
2022-04-27 23:09:58 +0300

    ADD ch5-3 unboxing error

---
+++ Ch5_3/Ch5_3/Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_3
+{
+    struct Point:IComparable
+    {
+        public int x;
+        public int y;
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format("{0} {1}", x, y);
+            return s;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return -1;
+        }
+
+        public void Change(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public int CompareTo(Point p2)
+        {
+            return -1;
+        }
+    }
+}
--- Ch5_3/Ch5_3/Program.cs
+++ Ch5_3/Ch5_3/Program.cs
@@ -10,6 +10,23 @@ namespace Ch5_3
     {
         static void Main(string[] args)
         {
+            Point p = new Point(1, 1);
+
+            Console.WriteLine(p);//(1,1)  boxing так как writeline принимает ссылочный тип
+
+            p.Change(2, 2);
+            Console.WriteLine(p);//(2,2)  boxing так как writeline принимает ссылочный тип, а предыддущий уже уничтожен
+
+            object o = p;//boxing 
+            Console.WriteLine(o);//(2,2)  
+
+            ((Point)o).Change(3, 3);//unboxing 
+            Console.WriteLine(o);//(2,2) так как в o не записалось ничего, мы разбоксили сделав новый объект в стеке и потом вернулись к тому что в куче
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
