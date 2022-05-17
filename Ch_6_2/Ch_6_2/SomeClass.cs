70250944d92c5caf9c820f44a7cb1ff62b17891e
Sabotage84 <sabotage84@mail.ru>
2022-05-17 22:39:13 +0300

    ADD ch6_2 static

---
+++ Ch_6_2/Ch_6_2/SomeClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_6_2
+{
+    class SomeClass
+    {
+
+        private class NestedClass { };
+
+        private const int SOME_CONST_FIELD = 1;
+
+        private readonly int someReadOnlyField = 2;
+
+        private static int SomeStaticField = 3;
+
+        static SomeClass()
+        {
+
+        }
+
+        public SomeClass()
+        {
+
+        }
+
+        public SomeClass(int x)
+        {
+
+        }
+
+        private string InstanceMeth()
+        {
+            return null;
+        }
+
+        private static void StaticMeth()
+        {
+
+        }
+
+        public void ParamsMeth(params int[] items)
+        {
+
+        }
+
+        public int SomeProp { get; set; }
+
+        public int SomeProperty
+        {
+            get { return 0; }
+            set { int a = value; }
+        }
+
+        public int this[string s]
+        {
+            get { return 0; }
+            set { }
+        }
+
+        public EventHandler SomeDelegate;
+        public event EventHandler SomeEvent;
+
+
+    }
+}
---
+++ Ch_6_2/Ch_6_2/SomeStaticClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_6_2
+{
+    static class SomeStaticClass
+    {
+        static SomeStaticClass()
+        {
+
+        }
+
+        public static void SomeMeth() { }
+
+        private static string someField;
+
+        public static string SomeField { get => someField; set => someField = value; }
+
+        public static int SomeProp { get; set; }
+
+
+    }
+}
