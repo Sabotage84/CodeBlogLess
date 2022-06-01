9e8b6f26879f5cd130c79ace138508aceec96122
Sabotage84 <sabotage84@mail.ru>
2022-06-01 21:38:18 +0300

    ADD ch-8 gelivery constructor

---
+++ Ch8/Ch8/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8
+{
+    class Class2 : Class1
+    {
+        //public Class2() нельзя делать такой конструктор так как у родителя только конструкторс параметром
+        //{
+
+        //}
+        public Class2(int i) : base(i)//надо так
+        {
+        }
+    }
+}
