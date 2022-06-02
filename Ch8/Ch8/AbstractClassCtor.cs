c5b4dad0e20c871d380629a292bcd7e01ee5c753
Sabotage84 <sabotage84@mail.ru>
2022-06-02 20:07:28 +0300

    ADD ch-8 abstract

---
+++ Ch8/Ch8/AbstractClassCtor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8
+{
+    abstract public class AbstractClassCtor
+    {
+        public AbstractClassCtor(int i)
+        {
+
+        }
+        public AbstractClassCtor()
+        {
+
+        }
+
+    }
+}
---
+++ Ch8/Ch8/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8
+{
+    class Class3:AbstractClassCtor
+    {
+        public Class3()
+        {
+
+        }
+    }
+}
