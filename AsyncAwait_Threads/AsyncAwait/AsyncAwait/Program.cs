ceb3e6851ecf5de613a307f62ce8281cffbacf76
Sabotage84 <sabotage84@mail.ru>
2022-07-08 22:48:14 +0300

    ADD async less simple asyncawait

---
+++ AsyncAwait_Threads/AsyncAwait/AsyncAwait/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DoWorkAsync();
+
+
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (j % 100000 == 0)
+                {
+                    Console.WriteLine("MAin");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static async Task DoWorkAsync()
+        {
+            await Task.Run(() => DoWork());
+        }
+
+        static void DoWork()
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (j % 100000 == 0)
+                {
+                    Console.WriteLine("DoWork");
+                }
+            }
+        }
+
+        static void DoWork2(object o)
+        {
+            int j = 0;
+            for (int i = 0; i < (int)o; i++)
+            {
+                j++;
+                if (j % 100000 == 0)
+                {
+                    Console.WriteLine("DoWork  2 2 2");
+                }
+            }
+        }
+    }
+}
