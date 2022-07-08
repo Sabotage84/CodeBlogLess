a07b43f73a7bfb2c05d61a294c8f82b96bd203bb
Sabotage84 <sabotage84@mail.ru>
2022-07-08 22:37:44 +0300

    ADD async less threads

---
+++ AsyncAwait_Threads/Threads_1/Threads_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(new ThreadStart(DoWork));
+            th1.Start();
+
+            Thread th2 = new Thread(new ParameterizedThreadStart(DoWork2));
+            th2.Start(int.MaxValue);
+
+
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (j % 100000 == 0)
+                {
+                    Console.WriteLine("Main");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void DoWork()
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (j%100000==0)
+                {
+                    Console.WriteLine("DoWork");
+                }
+            }
+        }
+
+        static void DoWork2(object o)
+        {
+            int j = 0;
+            for (int i = 0; i < (int)o; i++)
+            {
+                j++;
+                if (j % 100000 == 0)
+                {
+                    Console.WriteLine("DoWork  2 2 2");
+                }
+            }
+        }
+    }
+}
