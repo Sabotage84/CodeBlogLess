10ad592fa43cdab83590f9bff30c29bd610787a5
Sabotage84 <sabotage84@mail.ru>
2022-07-14 22:56:02 +0300

    ADD anonymous meth

---
+++ AnonymousMethAndLambda/AnonymousMeth/AnonymousMeth/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMeth
+{
+    class Program
+    {
+        delegate int MyHandler(int x);
+        static void Main(string[] args)
+        {
+            int r = 7;
+
+            MyHandler handler = delegate (int i)
+             {
+                 Console.WriteLine(i * i);
+                 return i * i;
+             };
+
+            handler += MyMeth;
+            
+            handler(r);
+            Console.ReadLine();
+        }
+
+        static int MyMeth(int i)
+        {
+            Console.WriteLine(i * i*i);
+            return i * i * i;
+        }
+    }
+}
