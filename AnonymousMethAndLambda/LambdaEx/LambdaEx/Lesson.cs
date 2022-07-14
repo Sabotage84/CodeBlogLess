cefa8f73ac34fbdf2a08a9ac0f93e3d2111cd4df
Sabotage84 <sabotage84@mail.ru>
2022-07-15 00:04:28 +0300

    ADD lambda

---
+++ AnonymousMethAndLambda/LambdaEx/LambdaEx/Lesson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaEx
+{
+    public class Lesson
+    {
+        public string Name { get; }
+
+        public DateTime Begin { get; private set; }
+
+        public event EventHandler<DateTime> Started;
+
+        public Lesson(string name)
+        {
+            Name = name;
+        }
+
+        public void Start()
+        {
+            Begin = DateTime.Now;
+            Started?.Invoke(this, Begin);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ AnonymousMethAndLambda/LambdaEx/LambdaEx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lesson ls = new Lesson("Test lambda");
+
+            ls.Started += (sender, date) =>
+              {
+                  Console.WriteLine(sender);
+                  Console.WriteLine(date);
+              };
+
+
+            ls.Start();
+
+
+            Console.ReadLine();
+        }
+    }
+}
