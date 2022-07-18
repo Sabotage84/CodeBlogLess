575ae5f2c5539d66075347046a0d9a05a9b0e87f
Sabotage84 <sabotage84@mail.ru>
2022-07-18 23:58:51 +0300

    ADD LINQ 1

---
+++ LINQ/LinqLesson/LinqLesson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                lst.Add(i);
+            }
+            var result = from item in lst
+                         where item < 500
+                         select item;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
