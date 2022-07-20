f928172ab4f3baee570988398088b49f4725a40d
Sabotage84 <sabotage84@mail.ru>
2022-07-20 23:46:08 +0300

    ADD LINQ 4

---
+++ LINQ/LinqLesson/LinqLesson/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLesson
+{
+    public class Product:IComparable
+    {
+        public string Name { get; set; }
+
+        public int Energy { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Product p = obj as Product;
+            return p.Energy.CompareTo(Energy);
+        }
+    }
+}
--- LINQ/LinqLesson/LinqLesson/Program.cs
+++ LINQ/LinqLesson/LinqLesson/Program.cs
@@ -10,24 +10,28 @@ namespace LinqLesson
     {
         static void Main(string[] args)
         {
-            List<int> lst = new List<int>();
+            List<Product> lst = new List<Product>();
 
+
+            Random rnd = new Random();
             for (int i = 0; i < 1000; i++)
             {
-                lst.Add(i);
+                var p = new Product { Name = "Продукт " + i, Energy = rnd.Next(10, 500) };
+
+                lst.Add(p);
             }
             var result = from item in lst
-                         where item < 500
+                         where item.Energy < 100
                          select item;
 
-            var result2 = lst.Where(item => item % 2 == 0);
+            var result2 = lst.Where(item => item.Energy % 2 == 0);
 
-            var result3 = lst.Where(item => item % 2 == 0).Where(x=>x%5==0).OrderByDescending(item=>item);
+            var result3 = lst.Where(item => item.Energy % 2 == 0).Where(x=>x.Energy%5==0).OrderByDescending(item=>item);
 
 
             foreach (var item in result3)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.Name + "   "+ item.Energy);
             }
             Console.ReadKey();
         }
