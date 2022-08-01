920b7ff6cbb0f0b3f21e9b14c8533e9bb2b4d3ed
Sabotage84 <sabotage84@mail.ru>
2022-08-01 22:53:29 +0300

    ADD ch 8-3 partial meth

---
+++ Ch8_3/Ch8_3/Base.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_3
+{
+    sealed partial class Base
+    {
+        string name;
+
+        partial void OnNameChanged(string newName);
+        
+
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+
+            set 
+            {
+                OnNameChanged(value.ToLower());
+                name = value;
+
+            }
+        }
+    }
+
+    sealed partial class Base
+    {
+        partial void OnNameChanged(string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                throw new ArgumentNullException(nameof(newName));
+            }
+        }
+    }
+}
