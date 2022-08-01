4e7d4e417d64713c2de73dcfe41af504c5dd6ea1
Sabotage84 <sabotage84@mail.ru>
2022-08-02 00:15:05 +0300

    ADD ch 9 method params

--- Ch_9/Ch_9/Program.cs
+++ Ch_9/Ch_9/Program.cs
@@ -8,8 +8,23 @@ namespace Ch_9
 {
     class Program
     {
+        static int value = 0;
         static void Main(string[] args)
         {
+            MyMeth(); //простой вызов метода все параметры по умолчанию
+            MyMeth(8, "X");//первые параметры по очереди заданы при вызове
+            MyMeth(7, g: Guid.NewGuid(), dt: DateTime.Now);//в разнобой поименно
+            MyMeth(value++, value++.ToString());//пример не очевидного пост инкремента
+            Console.WriteLine(value);//и далее
+            MyMeth(s: (value++).ToString(), x: value++);//и далее
+            Console.WriteLine(value);//и далее
+            Console.ReadKey();
+
+
+        }
+        public static void MyMeth(int x = 9, string s="A", DateTime dt=default, Guid g =new Guid())
+        {
+            Console.WriteLine($"x = {x}, s = {s}, dt = {dt}, guid = {g}");
         }
     }
 }
