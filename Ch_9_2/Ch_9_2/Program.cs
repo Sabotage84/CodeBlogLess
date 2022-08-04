a46e7b36fcca5eab54e5455e21ecdb5330afc911
Sabotage84 <sabotage84@mail.ru>
2022-08-05 00:41:21 +0300

    ADD ch9_2 out ref

---
+++ Ch_9_2/Ch_9_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x;
+            GetVAlue(out x);
+            Console.WriteLine(x);
+
+
+
+            int y = 7;
+            AddValue(ref y);
+            Console.WriteLine(y);
+
+
+
+            Console.ReadKey();
+
+        }
+
+        private static void AddValue(ref int y)
+        {
+            y += 10;
+        }
+
+        private static void GetVAlue(out int x)
+        {
+            x = 10;
+        }
+    }
+}
