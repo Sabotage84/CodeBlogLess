89f121382eff7ca9c08d432463a26d3e2562162f
Sabotage84 <sabotage84@mail.ru>
2022-08-05 01:27:59 +0300

    ADD ch9_2 out ref 3

--- Ch_9_2/Ch_9_2/Program.cs
+++ Ch_9_2/Ch_9_2/Program.cs
@@ -26,8 +26,41 @@ namespace Ch_9_2
             ChangeDogNAme(ref d, "Харлей");
             Console.WriteLine(d);
 
+
+            Dog d2;
+            CreateDog(out d2);
+            Console.WriteLine(d2);
+
+
+            var s1 = "CODE";
+            var s2 = "BLOGE";
+            Console.WriteLine(s1+s2);
+            Swop(ref s1, ref s2);
+            Console.WriteLine(s1+s2);
+
+
             Console.ReadKey();
 
+        }
+
+        private static void Swop(ref object s1, ref object s2)//обжект нельзя так менять,
+                                                              //передать в них не обжект не получится
+        {
+            var temp = s2;
+            s2 = s1;
+            s1 = temp;
+        }
+
+        private static void Swop<T>(ref T s1, ref T s2)
+        {
+            var temp = s2;
+            s2 = s1;
+            s1 = temp;
+        }
+
+        private static void CreateDog(out Dog d2)
+        {
+            d2 = new Dog("dog2");
         }
 
         private static void AddValue(ref int y)
