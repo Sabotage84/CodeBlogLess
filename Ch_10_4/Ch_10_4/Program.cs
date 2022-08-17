56ec3ab91408d0067032a66d1e2c75ff93f1c975
Sabotage84 <sabotage84@mail.ru>
2022-08-18 01:09:31 +0300

    ADD ch10-4 fin

--- Ch_10_4/Ch_10_4/Program.cs
+++ Ch_10_4/Ch_10_4/Program.cs
@@ -13,8 +13,15 @@ namespace Ch_10_4
             //Анонимный тип 
 
             var item = new { Name = "Ivan", Age = 22 };
-
+            Console.WriteLine(item);
             Console.WriteLine(item.Name);
+
+            var Name = "v2";
+            var Age = 22;
+            //тоже корректно
+            var item2 = new { Name, Age };
+            //анонимные типы не должны выходить за пределы класса
+
             Console.ReadLine();
         }
     }
