198836d0599fa5459d049ab0160ea0d693fab40b
Sabotage84 <sabotage84@mail.ru>
2022-08-18 00:39:28 +0300

    ADD ch10-4

---
+++ Ch_10_4/Ch_10_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_10_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Анонимный тип 
+
+            var item = new { Name = "Ivan", Age = 22 };
+
+            Console.WriteLine(item.Name);
+            Console.ReadLine();
+        }
+    }
+}
