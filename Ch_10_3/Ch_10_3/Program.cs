df49381f1ac7d9b0c2246ad9a990b194aec8d3ef
Sabotage84 <sabotage84@mail.ru>
2022-08-17 23:27:10 +0300

    ADD ch10-3 epmloyee

--- Ch_10_3/Ch_10_3/Program.cs
+++ Ch_10_3/Ch_10_3/Program.cs
@@ -10,6 +10,17 @@ namespace Ch_10_3
     {
         static void Main(string[] args)
         {
+            var employee = new Employee()
+            {
+                Name = "v1",
+                Age = 22
+            };
+            //эквивалентно
+            var employee2 = new Employee();
+            employee2.Name = "v2";
+            employee2.Age = 22;
+           
+
         }
     }
 }
