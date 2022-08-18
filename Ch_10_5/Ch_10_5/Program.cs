e22591ecbf61c7e6fe30e925b97453652c1af07b
Sabotage84 <sabotage84@mail.ru>
2022-08-19 01:07:12 +0300

    ADD ch10-5  fin

--- Ch_10_5/Ch_10_5/Program.cs
+++ Ch_10_5/Ch_10_5/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Dynamic;
 namespace Ch_10_5
 {
     class Program
@@ -11,6 +11,18 @@ namespace Ch_10_5
         static void Main(string[] args)
         {
             var explTuple = new Tuple<int, int, int>(1,10, 15);
+            Console.WriteLine(explTuple.Item1);//не читаемо
+
+            var tuple2 = Tuple.Create(1, 2, 3, "Ivan");
+
+            //другой подход более удобно читать но динамик-риск
+            dynamic e = new ExpandoObject();
+            e.x = 3;
+            e.y = 5;
+            e.name = "EO";
+
+
+
             Console.ReadLine();
         }
     }
