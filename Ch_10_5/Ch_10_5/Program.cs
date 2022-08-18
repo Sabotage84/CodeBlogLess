de95a00eac3745855a65d9c54dafaac920c5d77e
Sabotage84 <sabotage84@mail.ru>
2022-08-19 00:13:14 +0300

    ADD ch10-5  tuple

---
+++ Ch_10_5/Ch_10_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_10_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var explTuple = new Tuple<int, int, int>(1,10, 15);
+            Console.ReadLine();
+        }
+    }
+}
