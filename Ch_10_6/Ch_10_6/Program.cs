1958579c30285b345340c5e28ad72adefd2980c1
Sabotage84 <sabotage84@mail.ru>
2022-08-29 22:46:36 +0300

    ADD ch10-6 2

--- Ch_10_6/Ch_10_6/Program.cs
+++ Ch_10_6/Ch_10_6/Program.cs
@@ -10,6 +10,21 @@ namespace Ch_10_6
     {
         static void Main(string[] args)
         {
+            var vt1 = (1, "Value", "Tuple");
+
+            Console.WriteLine(vt1.Item2+" " + vt1.Item3);
+            Console.WriteLine();
+
+            ValueTuple<int, string, string> vt2 = (1, "Value", "Tuple 2");
+            Console.WriteLine(vt2.Item2 + " " + vt2.Item3);
+            Console.WriteLine();
+
+
+
+
+            Console.ReadLine();
+
+
         }
     }
 }
