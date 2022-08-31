3f44d0463a3b572d636370be5ba9ce10af5c7179
Sabotage84 <sabotage84@mail.ru>
2022-09-01 00:09:11 +0300

    ADD ch11 event

---
+++ Ch_11/Ch_11/MailManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_11
+{
+    internal class MailManager
+    {
+        public event EventHandler<NewMailEventArgs> NewMail;
+
+        protected virtual void OnNewMail(NewMailEventArgs e)
+        {
+            NewMail?.Invoke(this, e);
+        }
+
+        public void SimulateNewMail(string from, string to , string subject)
+        {
+
+        }
+    }
+}
