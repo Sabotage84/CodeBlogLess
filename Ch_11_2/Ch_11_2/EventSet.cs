a82e99646ecf86b4876952b3a9767a9647689e24
Sabotage84 <sabotage84@mail.ru>
2022-09-09 23:46:37 +0300

    ADD Ch11-2 fin

---
+++ Ch_11_2/Ch_11_2/EventSet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ch_11_2
+{
+    public class EventKey { }// любой класс даже пустой для ключа в словаре
+    class EventSet
+    {
+        private readonly Dictionary<EventKey, Delegate> events = new Dictionary<EventKey, Delegate>();
+
+        public void Add(EventKey eventKey, Delegate handler)
+        {
+            Monitor.Enter(events);
+            if (events.TryGetValue(eventKey, out var d))
+            {
+                events[eventKey] = Delegate.Combine(d, handler);
+            }
+            else
+            {
+                events.Add(eventKey, handler);
+            }
+            Monitor.Exit(events);
+
+        }
+
+        public void Remove(EventKey eventKey, Delegate handler)
+        {
+            Monitor.Enter(events);
+            if (events.TryGetValue(eventKey, out var d))
+            {
+                d = Delegate.Remove(d, handler);
+                if (d!=null)
+                {
+                    events[eventKey] = d;
+                }
+                else
+                {
+                    events.Remove(eventKey);
+                }
+            }
+            Monitor.Exit(events);
+
+        }
+
+        public void Raise(EventKey eventKey, object sender, EventArgs e)
+        {
+            Monitor.Enter(events);
+            if (events.TryGetValue(eventKey, out var d))
+            {
+                d.DynamicInvoke(new object[] { sender, e });
+            }
+            Monitor.Exit(events);
+        }
+    }
+}
---
+++ Ch_11_2/Ch_11_2/TypeWithLotsEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_11_2
+{
+    public class FooEventArgs : EventArgs { }
+    class TypeWithLotsEvents
+    {
+        protected EventSet EventSet { get; } = new EventSet();
+
+        protected static readonly EventKey fooEventKey = new EventKey();
+
+        public event EventHandler<FooEventArgs> Foo
+        {
+            add { EventSet.Add(fooEventKey, value); }
+            remove { EventSet.Remove(fooEventKey, value); }
+        }
+
+        protected virtual void OnFoo(FooEventArgs e)
+        {
+            EventSet.Raise(fooEventKey, this, e);
+        }
+
+        public void SimulateFoo()
+        {
+            OnFoo(new FooEventArgs());
+        }
+    }
+}
