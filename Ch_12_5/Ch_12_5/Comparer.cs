2602df65247659afdcfe627d1a5e4bd7e7b8b5ae
Sabotage84 <sabotage84@mail.ru>
2022-09-21 01:47:17 +0300

    ADD Ch12.5 part 2

---
+++ Ch_12_5/Ch_12_5/Comparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_12_5
+{
+    public class Comparer<TItem> where TItem: IComparable
+    {
+
+        //можно сравнивать TItem
+    }
+}
