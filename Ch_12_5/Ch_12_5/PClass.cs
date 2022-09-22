e808be450509b3e97cce10055ea0f303a20c422a
Sabotage84 <sabotage84@mail.ru>
2022-09-22 23:55:54 +0300

    ADD Ch12.5 part 3

--- Ch_12_5/Ch_12_5/PClass.cs
+++ Ch_12_5/Ch_12_5/PClass.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Ch_12_5
 {
-    public class PClass<TKey,TValue>
+    public class PClass<TKey,TValue>//CLR не знает какие типы будут тут, открытый тип
+                                    //(нельзя создавать экз. без указания типа)
     {
         TKey i;
 
