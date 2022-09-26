b86bce968388c4e70635fc5cc1bcf69f3721865b
Sabotage84 <sabotage84@mail.ru>
2022-09-26 21:58:27 +0300

    ADD Ch12.5 part 5

--- Ch_12_5/Ch_12_5/Program.cs
+++ Ch_12_5/Ch_12_5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +16,19 @@ namespace Ch_12_5
             //  public class List<T> : IList<T>, ICollection<T>, IEnumerable<T>, IEnumerable, IList, ICollection, IReadOnlyList<T>, IReadOnlyCollection<T>
 
             var d1 = new Dictionary<string, Guid>();//эти типы несовместимы
-            var d2 = new DictionaryString<Guid>();//так как разное наследование
-            
+            var d2 = new DictionaryString<Guid>();//так как разное наследование 
+
+
+            //если есть такая колбаса
+            //List<Dictionary<Dictionaryint,string>, Dictionary<char, Stream>>>
+            //проще сделать класс смотри dich
+            //и уже использовать так
+            Dich d3 = new Dich();
         }
+
+        public class Dich : List<Dictionary<Dictionary<int, string>, Dictionary<char, Stream>>>
+            { 
+        }
+
     }
 }
