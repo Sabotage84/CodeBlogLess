ff185321777550d0a3093430e141a7ed9fd622c1
Sabotage84 <sabotage84@mail.ru>
2022-10-14 22:55:01 +0300

    ADD FactoryMethod part 2

---
+++ FactoryMethod/FactoryMethod/IMessanger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public interface IMessanger
+    {
+        string UserName { get; }
+
+        string Password { get; }
+
+        bool Connected { get; }
+
+        IMessage CreateMessage(string text, string source, string target);
+
+        bool Authorize();
+    }
+}
---
+++ FactoryMethod/FactoryMethod/MessageBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public abstract class MessageBase : IMessage
+    {
+        public string Text { get; protected set; }
+
+        public string Target { get; }
+
+        public string Source { get; }
+
+        public MessageBase(string text, string source, string target)
+        {
+
+            #region Проверяем входные данные на валидность
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentNullException(nameof(text), "Текст сообщения не может быть пустым.");
+            }
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                throw new ArgumentNullException(nameof(source), "Имя отправителя не может быть пустым.");
+            }
+            if (string.IsNullOrWhiteSpace(target))
+            {
+                throw new ArgumentNullException(nameof(target), "Имя получателя не может быть пустым.");
+            }
+            //if (text.Length > 140)
+            //{
+            //    throw new ArgumentException(nameof(text), "Текст не может быть больше 140 символов.");
+            //}
+
+            #endregion
+            Text = text;
+            Source = source;
+            Target = target;
+        }
+
+        public abstract void Send();
+       
+    }
+}
