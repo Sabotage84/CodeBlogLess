275dbb0adb8499f49893c5790c5263140379f8be
Sabotage84 <sabotage84@mail.ru>
2022-10-26 23:12:14 +0300

    ADD linkedList part 1

---
+++ LinkedList/LinkedList/Model/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Model
+{
+    public class Item<T>
+    {
+        private T data = default(T);
+        private Item<T> next = null;
+    }
+}
