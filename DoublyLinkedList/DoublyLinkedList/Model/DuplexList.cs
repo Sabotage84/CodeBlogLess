e523ad9be4e769260bef4a2247822136af550c45
Sabotage84 <sabotage84@mail.ru>
2022-11-10 21:32:49 +0300

    ADD doubly linked list v2

---
+++ DoublyLinkedList/DoublyLinkedList/Model/DuplexList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList.Model
+{
+    public class DuplexList<T>
+    {
+        public DuplexItem<T> Head {get; set;}
+
+        public DuplexItem<T> Tail { get; set; }
+
+        public int Count { get; set; }
+
+        public DuplexList()
+        {
+
+        }
+
+        public DuplexList(T data)
+        {
+            var item = new DuplexItem<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+    }
+}
