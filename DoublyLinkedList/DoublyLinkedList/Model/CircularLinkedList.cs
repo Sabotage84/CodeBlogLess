e17c3ed3a6ad21776d03b6d7c7df2fb5a742b6e0
Sabotage84 <sabotage84@mail.ru>
2022-11-16 23:03:33 +0300

    ADD doubly linked list v8

---
+++ DoublyLinkedList/DoublyLinkedList/Model/CircularLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList.Model
+{
+    public class CircularLinkedList<T>
+    {
+        public DuplexItem<T> Head { get; set; }
+        public int Count { get; set; }
+
+        public CircularLinkedList()
+        {
+
+        }
+
+        public CircularLinkedList(T data)
+        {
+            Head = new DuplexItem<T>(data);
+            Count = 1;
+        }
+
+    }
+}
