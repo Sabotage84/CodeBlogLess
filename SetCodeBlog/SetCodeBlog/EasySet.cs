aa6f194e240b2e67545006bbecd8148c5f843180
Sabotage84 <sabotage84@mail.ru>
2022-11-18 23:34:17 +0300

    ADD SET v1

---
+++ SetCodeBlog/SetCodeBlog/EasySet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetCodeBlog
+{
+    public class EasySet<T>
+    {
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+
+        public EasySet()
+        {
+
+        }
+        public EasySet(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Add(T item)
+        {
+            foreach (var i in items)
+            {
+                if (i.Equals(item))
+                {
+                    return;
+                }
+            }
+            items.Add(item);
+        }
+    }
+}
