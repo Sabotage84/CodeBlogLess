172a5816a1b9c99cc952600a6b4edb7ed77e2a84
sabotage84 <sabotage84@mail.ru>
2022-11-28 20:08:47 +0300

    ADD HashTable v3

--- HashTableCB/HashTableCB/HashTAbleCodeBlog.cs
+++ HashTableCB/HashTableCB/HashTAbleCodeBlog.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HashTableCB
+﻿namespace HashTableCB
 {
-    public class HashTAbleCodeBlog
+    public class HashTableCodeBlog<T>
     {
+        private T[] items;
+
+        public HashTableCodeBlog(int size)
+        {
+            items = new T[size];
+
+        }
+
+        public void Add(T item)
+        {
+            var key = GetHash(item);
+            items[key] = item;
+        }
+
+        public bool Search(T item)
+        {
+            var key = (int)GetHash(item);
+            return items[key].Equals(item);
+        }
+
+        private int GetHash(T item)
+        {
+            return item.ToString().Length % items.Length - 1;
+        }
+
     }
 }
