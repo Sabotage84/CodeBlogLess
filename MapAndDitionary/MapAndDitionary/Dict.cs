cf2662b9ad246bad32d50204f24ac43aa4765ad0
sabotage84 <sabotage84@mail.ru>
2022-12-02 23:50:56 +0300

    Add gict v2

---
+++ MapAndDitionary/MapAndDitionary/Dict.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapAndDitionary
+{
+    internal class Dict<TKey, TValue>
+    {
+        private int size = 100;
+        private Item<TKey, TValue>[] Items ;
+
+        public Dict()
+        {
+            Items = new Item<TKey, TValue>[100];
+        }
+
+        public void Add(Item<TKey, TValue> item) 
+        {
+            var hash = GetHash(item.Key);
+
+            if (Items[hash]==null) 
+            {
+                Items[hash] = item;
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+                    if (Items[i].Key.Equals(item.Key))
+                    {
+                        return;
+                    }
+
+                    if (Items[i]==null)
+                    {
+                        Items[i]=item;
+                        placed = true;
+                        break;
+                    }
+
+                }
+                if (!placed)
+                {
+                    for (var i = 0; i < hash; i++)
+                    {
+                        if (Items[i].Key.Equals(item.Key))
+                        {
+                            return;
+                        }
+
+                        if (Items[i] == null)
+                        {
+                            Items[i] = item;
+                            placed = true;
+                            break;
+                        }
+
+                    }
+                }
+                if(!placed)
+                {
+                    throw new Exception("Dict is full!");  
+                }
+            }
+
+        }
+
+        public void Remove(TKey key)
+        {
+            var hash = GetHash(key);
+            if (Items[hash].Key.Equals(key))
+            {
+                Items[hash] = null;
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+                    if (Items[i].Key.Equals(key))
+                    {
+                        Items[hash] = null;
+                        return;
+                    }
+
+                    if (Items[i] == null)
+                    {
+
+                        return;
+                    }
+
+                }
+                if (!placed)
+                {
+                    for (var i = 0; i < hash; i++)
+                    {
+                        if (Items[i].Key.Equals(key))
+                        {
+
+                            Items[hash] = null;
+                            return;
+                        }
+
+                        if (Items[i] == null)
+                        {
+                            return;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public TValue Search(TKey key)
+        {
+            var hash = GetHash(key);
+            if (Items[hash].Key.Equals(key))
+            {
+                return Items[hash].Value;
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+                    if (Items[i].Key.Equals(key))
+                    {
+                        return Items[hash].Value;
+                    }
+
+                    if (Items[i] == null)
+                    {
+                        return default(TValue);
+                    }
+
+                }
+                if (!placed)
+                {
+                    for (var i = 0; i < hash; i++)
+                    {
+                        if (Items[i].Key.Equals(key))
+                        {
+                            return Items[hash].Value;
+                        }
+
+                        if (Items[i] == null)
+                        {
+                            return default(TValue);
+                        }
+
+                    }
+                } 
+                return default(TValue);
+            }
+        }
+
+        private int GetHash(TKey key)
+        {
+            return key.GetHashCode()%size;
+        }
+
+
+    }
+}
