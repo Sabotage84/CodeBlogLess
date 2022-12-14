4b988d0cff51cd2466e12161d2657a90f0c7c3e1
sabotage84 <sabotage84@mail.ru>
2022-12-14 22:44:11 +0300

    Add EseayQueue v2

--- QueueDeque/QueueDeque/Model/EseayQueue.cs
+++ QueueDeque/QueueDeque/Model/EseayQueue.cs
@@ -6,9 +6,41 @@ using System.Threading.Tasks;
 
 namespace QueueDeque.Model
 {
-    internal class EseayQueue
+    internal class EseayQueue<T>
     {
+        private List<T> items = new List<T>();
 
+        private T Head  => items.Last();
+
+        private T Tail => items.First();
+
+        public int Count => items.Count;
+
+        public EseayQueue()
+        {
+
+        }
+        public EseayQueue(T data)
+        {
+            items.Add(data);
+        }
+
+        public void Enqueue(T data)
+        {
+            items.Insert(0, data);
+        }
+
+        public T Dequeue(T data)
+        {
+            T res = Head;
+            items.Remove(Head);
+            return res;
+        }
+
+        public T Peek()
+        {
+            return Head;
+        }
 
     }
 }
