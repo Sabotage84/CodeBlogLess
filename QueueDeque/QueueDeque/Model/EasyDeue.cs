deaa260ae78c6d74e754ae4779e44d8761eca46e
sabotage84 <sabotage84@mail.ru>
2022-12-16 22:48:17 +0300

    Add easydeue

---
+++ QueueDeque/QueueDeque/Model/EasyDeue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueDeque.Model
+{
+    internal class EasyDeue<T>
+    {
+        private List<T> items = new List<T>();
+
+        public T Head => items.Last();
+
+        public T Tail => items.First();
+
+        int Count => items.Count();
+
+        public EasyDeue() { }
+
+        public EasyDeue(T data)
+        {
+            items.Add(data);
+        }
+        public void PushBack(T data)
+        {
+            items.Insert(0, data);
+        }
+
+        public T PopBack()
+        {
+            var res=Tail;
+            items.Remove(Tail);
+            return res;
+
+        }
+
+        public T PopFront()
+        {
+            var res = Head;
+            items.Remove(Head);
+
+            return res;
+        }
+
+        public void PushFront(T data)
+        {
+            items.Add(data);
+        }
+
+        public T PeekBack()
+        {
+            return Tail;
+        }
+
+        public T PeekFront()
+        {
+            return Head;
+        }
+
+
+    }
+
+}
