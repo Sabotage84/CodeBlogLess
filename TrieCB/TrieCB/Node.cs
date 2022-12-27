8d4e3be9865509d492e7617bffef91b10b4088f7
sabotage84 <sabotage84@mail.ru>
2022-12-27 22:50:00 +0300

    Add trie v2

---
+++ TrieCB/TrieCB/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieCB
+{
+    internal class Node<T>
+    {
+        public string Key { get; set; }
+
+        public T Data { get; set; }
+
+        public bool IsWord { get; set; }
+        
+
+
+    }
+}
