0669529cb7e1eeacd39a90201ddb226b61f51107
sabotage84 <sabotage84@mail.ru>
2023-02-03 00:04:55 +0300

    Add BinaryHeap v0-1

---
+++ BinaryHeapCB/BinaryHeapCB/BinaryHeap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeapCB
+{
+    internal class BinaryHeap<T>
+    {
+        private List<T> items= new List<T>();
+        public int Count { get { return items.Count; } }
+
+        public T GetMax()
+        {
+            return items[0];
+        }
+    }
+}
