aae73f41fc878942beab0a84459701c1508b2f49
sabotage84 <sabotage84@mail.ru>
2023-02-06 23:21:02 +0300

    Add binary heap add

--- BinaryHeapCB/BinaryHeapCB/BinaryHeap.cs
+++ BinaryHeapCB/BinaryHeapCB/BinaryHeap.cs
@@ -6,14 +6,41 @@ using System.Threading.Tasks;
 
 namespace BinaryHeapCB
 {
-    internal class BinaryHeap<T>
+    internal class BinaryHeap
     {
-        private List<T> items= new List<T>();
+        private List<int> items= new List<int>();
         public int Count { get { return items.Count; } }
 
-        public T GetMax()
+        public int GetMax()
         {
+            if (Count > 0) 
+            { 
             return items[0];
+        
+            }
+            else
+            {
+                return default(int);
+            }
+        }
+
+        public void Add(int item)
+        {
+            items.Add(item);
+
+            var currentIndex = Count-1;
+            var parentIndex = (currentIndex - 1) / 2;
+
+            while (currentIndex>0 && items[parentIndex] < items[currentIndex])
+            {
+                var temp = items[currentIndex];
+                items[currentIndex] = items[parentIndex];
+                items[parentIndex] = temp;
+
+                currentIndex = parentIndex;
+                parentIndex = (currentIndex - 1) / 2;
+            }
+
         }
     }
 }
