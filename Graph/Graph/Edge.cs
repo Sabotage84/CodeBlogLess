3c0aa1b86a964c08882653c820158ab3048d5876
Sabotage84 <sabotage84@mail.ru>
2023-02-15 23:15:55 +0300

    ADD Graph v0-2

---
+++ Graph/Graph/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class Edge
+    {
+        public Vertex To { get; set; }
+        public Vertex From { get; set; }
+        
+        public bool Oriented { get; set; }
+        public int Weight { get; set; }
+
+
+
+    }
+}
