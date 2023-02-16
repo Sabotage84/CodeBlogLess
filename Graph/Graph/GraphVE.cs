3e11ea6f0f31cfa20249ecec0532006b6f24090b
sabotage84 <sabotage84@mail.ru>
2023-02-17 00:21:58 +0300

    Add graph v1

--- Graph/Graph/GraphVE.cs
+++ Graph/Graph/GraphVE.cs
@@ -11,6 +11,20 @@ namespace Graph
         List<Vertex> Vertexes = new List<Vertex>();
         List<Edge> Edges = new List<Edge>();
 
+        public void AddVertex(Vertex v)
+        {
+
+            Vertexes.Add(v);
+        }
+
+        public void Remove(Vertex v) { }
+
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            Edges.Add(new Edge(from,to));
+        }
+
+
         public int[,] GetMatrix()
         {
             var matrix = new int[Vertexes.Count, Vertexes.Count];
@@ -21,11 +35,40 @@ namespace Graph
                 var col = item.To.Number;
 
                 matrix[row, col] = item.Weight;
-
+                matrix[col, row] = item.Weight;
             }
 
 
             return matrix;
+        }
+
+        public void ShowMatrix()
+        {
+            var matrix = GetMatrix();
+            for (int i = 0; i < Vertexes.Count; i++)
+            {
+                if (i == 0)
+                {
+                    for (int l = 0; l < Vertexes.Count; l++)
+                    {
+                        if (l == 0)
+                        {
+                            Console.Write("  ");
+                        }
+                        Console.Write((l + 1) + " ");
+                    }
+                    Console.WriteLine();
+                }
+                for (int j = 0; j < Vertexes.Count; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write((i+1)+" ");
+                    }
+                    Console.Write(matrix[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
