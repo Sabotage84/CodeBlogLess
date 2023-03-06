2d6d5ab55a8d32aced0a050cbbeaae5a30ccd150
sabotage84 <sabotage84@mail.ru>
2023-03-06 22:59:50 +0300

    Add practice day 1-5

---
+++ Practice1/CodeBlogFit.BL/Model/Gender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFit.BL.Model
+{
+    internal class Gender
+    {
+        public string Name { get; }
+
+        public Gender(string name) 
+        {
+            Name = name;
+        }
+    }
+}
