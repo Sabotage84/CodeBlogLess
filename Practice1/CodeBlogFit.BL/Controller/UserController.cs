6401cbe761704699b047b6cbf17ff729ef4581dd
Sabotage84 <sabotage84@mail.ru>
2023-03-28 21:48:28 +0300

    Add practice day 1-16

--- Practice1/CodeBlogFit.BL/Controller/UserController.cs
+++ Practice1/CodeBlogFit.BL/Controller/UserController.cs
@@ -8,16 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CodeBlogFit.BL.Controller
-{
+{       
+    /// <summary>
+    ////Контроллер пользователя
+    /// </summary>
     internal class UserController
     {
+        /// <summary>
+        ////Создание контроллера пользователя
+        /// </summary>
+        /// <param name="user">Пользователь приложения. </param>
+        /// <exception cref="ArgumentNullException"></exception>
         public UserController(User user) 
         {
             User = user ?? throw new ArgumentNullException("Пользователь не может быть равен NULL!", nameof(user));
         }
 
         public User User { get; }
-
+        /// <summary>
+        /// Сохранить данные пользователя
+        /// </summary>
         public void Save()
         {
             var formatter = new BinaryFormatter();
@@ -27,25 +37,24 @@ namespace CodeBlogFit.BL.Controller
                 formatter.Serialize(fs, User);
             }
         }
-
+        /// <summary>
+        /// Загрузить данные пользователя
+        /// </summary>
+        /// <returns>Пользователь приложения.</returns>
+        /// <exception cref="FileLoadException"></exception>
         public User Load()
         {
             var formatter = new BinaryFormatter();
             
             using(var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                if(formatter.Deserialize(fs) is User user)
-                {
-                    return user;
-                }
-                else
-                {
-                    throw new FileLoadException();
-                }
-
+             return formatter.Deserialize(fs) as User;
             }
-            
+
+
         }
+            
+        
 
     }
 }
