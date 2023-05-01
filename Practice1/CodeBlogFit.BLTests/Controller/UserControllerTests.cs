6d9dc1f99ae517fe268d5552835a3a48f5671fba
Sabotage84 <sabotage84@mail.ru>
2023-05-01 22:38:16 +0300

    Add practice day 1-31

---
+++ Practice1/CodeBlogFit.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlogFit.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFit.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void UserControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UserControllerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestSowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestShowAllTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
